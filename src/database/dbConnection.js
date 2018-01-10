483d9b8d594b06300ea57fa5113644bb46902fc2
Lucy King <lucy.m.king92@gmail.com>
2018-01-10 16:57:13 +0000

    Edited db_connections and ran dbBuild.js

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -1,33 +1,31 @@
 const pgp = require('pg-promise')();
-const env = require('env2')('./config.env');
 const url = require('url');
+require('env2')('config.env');
 
-const params = url.parse(process.env.DATABASE_URL);
 
+let DB_URL = process.env.DATABASE_URL;
+// if(process.env.NODE_ENV==='test'){
+//   DB_URL=process.env.TEST_DATABASE_URL;
+// }
+
+if (!DB_URL) {
+  throw new Error('Environment variable DATABASE_URL must be set');
+}
+
+const params = url.parse(DB_URL);
 
 const [username, password] = params.auth.split(':');
 
-const herokuDB = {
+const options = {
   host: params.hostname,
   port: params.port,
   database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2,
-  ssl: true,
+  max: process.env.DB_MAX_CONNECTIONS || 2
 };
 
-if (username) { herokuDB.user = username; }
-if (password) { herokuDB.password = password; }
+if (username) { options.user = username; }
+if (password) { options.password = password; }
 
-const localDB = {
-  host: 'localhost',
-  port: 5432,
-  database: 'upshop',
-// Below needs to have your own details in!
-  user: 'master',
-  password: 'password',
-};
+options.ssl = (options.host !== 'localhost');
 
-const connection = process.env.NODE_ENV === 'production' ? herokuDB : localDB;
-
-const db = pgp(connection);
-module.exports = db; 
+module.exports = pgp(options);
