862f17af4315d144fb405ff976f34412f2b0c4f1
Lex <alexisjaynedefoe@gmail.com>
2018-01-10 14:44:10 +0000

    :construction: finish build script

--- src/database/dbBuild.sql
+++ src/database/dbBuild.sql
@@ -3,59 +3,58 @@ BEGIN;
 DROP TABLE IF EXISTS places, users, standards, categories, comments, votes, standard_connections, category_connections;
 
 CREATE TABLE IF NOT EXISTS places (
-    place_id SERIAL PRIMARY KEY,
-    place_name VARCHAR(200) NOT NULL,
-    place_lat_long VARCHAR(50) NOT NULL,
-    place_hours VARCHAR(56) NOT NULL,
-    place_desc VARCHAR(200) NOT NULL,
-    place_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    lat_long VARCHAR(50) NOT NULL,
+    hours VARCHAR(56) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    user_create TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE IF NOT EXISTS categories (
-    cat_name VARCHAR(50) NOT NULL,
-    cat_desc VARCHAR(200) NOT NULL,
-    cat_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(200) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS standards (
-    standard_id SERIAL PRIMARY KEY,
-    standard_name VARCHAR NOT NULL,
-    standard_desc VARCHAR NOT NULL,
-    standard_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS comments (
-    comment_id SERIAL PRIMARY KEY,
-    place_id INT REFERENCES places(place_id),
-    user_id INT REFERENCES users(user_id),
+    id SERIAL PRIMARY KEY,
+    place_id INT REFERENCES places(id),
+    user_id INT REFERENCES users(id),
     comment VARCHAR(300) NOT NULL,
-    comment_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE IF NOT EXISTS votes (
-    vote_id SERIAL PRIMARY KEY,
-    user_id REFERENCES users(user_id),
-    place_id REFERENCES places(place_id)
-)
+    id SERIAL PRIMARY KEY,
+    user_id REFERENCES users(id),
+    place_id REFERENCES places(id)
+);
 
 CREATE TABLE IF NOT EXISTS standard_connections (
      id SERIAL PRIMARY KEY,
-     place_id INT REFERENCES places(place_id),
-     standard_id INT REFERENCES standards(standard_id)
-)
+     place_id INT REFERENCES places(id),
+     standard_id INT REFERENCES standards(id)
+);
 
 CREATE TABLE IF NOT EXISTS category_connections (
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id),
-    place_id INT REFERENCES places(place_id)
-)
+    user_id INT REFERENCES users(id),
+    category_id INT REFERENCES categories(id)
+);
 
 
 /*insert some dummy data here */
