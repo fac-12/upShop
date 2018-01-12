aa18f8cee9090ed1c71b63a731d949d6f6ae1a97
Lex <alexisjaynedefoe@gmail.com>
2018-01-12 11:48:45 +0000

    set up local test database

--- src/tests/queries.test.js
+++ src/tests/queries.test.js
@@ -2,16 +2,18 @@ const test = require('tape');
 const runDbBuild = require('../database/dbBuild.js');
 const queries = require('../controllers/queries');
 
+
+
+test("tape is working", t => {
+  t.equals(1,1, "one equals one");
+  t.end();
+});
+
 test('getPlace', (t) => {
   runDbBuild((err, res) => {
-    queries.getPlace((error, responseRows) => {
-      if (error) {
-        console.log(err);
-      } else {
-        console.log(responseRows);
-        t.equals(typeof responseRows, 'object', 'response should be an object');
+    queries.getPlace('Second Shot').then(res => {
+        t.equals(typeof res, 'object', 'response should be an object');
         t.end();
-      }
+      });
     });
   });
-});
