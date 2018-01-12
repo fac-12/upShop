efce9e049bf2b182564535eeb223509072de7dbb
Lucy King <lucy.m.king92@gmail.com>
2018-01-12 16:14:08 +0000

    edit dbConnections

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -13,21 +13,19 @@ if (process.env.TRAVIS === true) {
 	}
 }
 
-else if (!DB_URL &&  process.env.TRAVIS === false) {
-  throw new Error('Environment variable DATABASE_URL must be set');
+else if (process.env.NODE_ENV === 'test') {
+  DB_URL = process.env.DB_URLTEST;
 }
 
-else { 
-	
-	if (process.env.NODE_ENV === 'test') {
-  DB_URL = process.env.DB_URLTEST;
+else if (!DB_URL &&  process.env.TRAVIS === false) {
+  throw new Error('Environment variable DATABASE_URL must be set');
 }
 
 const params = url.parse(DB_URL);
 
 const [username, password] = params.auth.split(':');
 
-options = {
+const options = {
   host: params.hostname,
   port: params.port,
   database: params.pathname.split('/')[1],
@@ -38,6 +36,5 @@ if (username) { options.user = username; }
 if (password) { options.password = password; }
 
 options.ssl = (options.host !== 'localhost');
-}
 
 module.exports = pgp(options);
