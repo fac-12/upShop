746f1d20fb08dce58c302441189c7be7a02fe90d
Lucy King <lucy.m.king92@gmail.com>
2018-01-14 22:33:52 +0000

    Travis DB - changed if/else statements

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -13,47 +13,32 @@ if (process.env.TRAVIS === true) {
 	}
 }
 
-else if (process.env.NODE_ENV === 'test') {
-  DB_URL = process.env.DB_URLTEST;
-
-  const params = url.parse(DB_URL);
-  const [username, password] = params.auth.split(':');
-
-options = {
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2
-};
-
-if (username) { options.user = username; }
-if (password) { options.password = password; }
-
-options.ssl = (options.host !== 'localhost');
-
-}
-
 else if (!DB_URL &&  process.env.TRAVIS === false) {
   throw new Error('Environment variable DATABASE_URL must be set');
 }
 
 else {
 
-const params = url.parse(DB_URL);
+	if (process.env.NODE_ENV === 'test') {
+  		DB_URL = process.env.DB_URLTEST;
+  	}
 
-const [username, password] = params.auth.split(':');
+  const params = url.parse(DB_URL);
+  const [username, password] = params.auth.split(':');
 
-options = {
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2
-};
+	options = {
+ 	 host: params.hostname,
+ 	 port: params.port,
+ 	 database: params.pathname.split('/')[1],
+ 	 max: process.env.DB_MAX_CONNECTIONS || 2
+	};
 
 if (username) { options.user = username; }
 if (password) { options.password = password; }
 
 options.ssl = (options.host !== 'localhost');
+
 }
+
 
 module.exports = pgp(options);
