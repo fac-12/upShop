a75302b21603934638389b414f0d83129d72b4bf
Lucy King <lucy.m.king92@gmail.com>
2018-01-15 10:56:35 +0000

    made navbar visible on place details page

--- src/controllers/place.js
+++ src/controllers/place.js
@@ -9,7 +9,7 @@ exports.get = (req, res) => {
       console.log(placeDetails);
       let placeResults = placeDetails[0]
       let values = placeDetails;
-      res.render('placeDetails', { placeResults, values })
+      res.render('placeDetails', { placeResults, values, layout: 'navHomeBack'})
     })
     .catch((err) => {
       console.log(err);
