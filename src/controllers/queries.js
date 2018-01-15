efbf54a538d88f99322439bc2a416b8dc863f5b0
Lex <alexisjaynedefoe@gmail.com>
2018-01-15 16:31:47 +0000

    update getPlace query, add getValues query

--- src/controllers/queries.js
+++ src/controllers/queries.js
@@ -6,18 +6,22 @@ const catResults = (category) => {
 
 }
 
-
 const checkPlace = (formData) => {
   return db.query(`
   SELECT CASE WHEN EXISTS (SELECT name FROM places WHERE name= ($1) AND address=($2) AND postcode=($3)) THEN CAST (1 AS BIT) ELSE CAST(0 AS BIT) END`, [formData.name, formData.address, formData.postcode]);
 };
 
 const getPlace = (name) => {
-  return db.query('select standards.name as standards, places.name, places.lat_long, places.postcode, places.hours, places.website, places.description from standards inner join standard_connections on standard_connections.standard_id = standards.id, places inner join category_connections on category_connections.place_id = places.id where standard_connections.place_id = 1 AND category_connections.category_id = 3');
-};
+  return db.query(`SELECT * FROM places WHERE name = ($1)`, [name]);
+}
+
+const getValues = (id) => {
+  return db.query(`SELECT array_agg(standards.name) as standard FROM standards INNER JOIN standard_connections ON standard_connections.standard_id = standards.id WHERE standard_connections.place_id = ($1)`, [id]);
+}
 
 module.exports = {
   catResults,
   checkPlace,
   getPlace,
+  getValues,
 };
