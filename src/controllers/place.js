7738f35851853213b8c5035299b40e6c889362e7
Lucy King <lucy.m.king92@gmail.com>
2018-01-16 12:06:47 +0000

    edited place query to remove nesting and add error handling

--- src/controllers/place.js
+++ src/controllers/place.js
@@ -1,32 +1,34 @@
 const queries = require('./queries');
 
 exports.get = (req, res) => {
+  let placeResults; 
+  let placeResultsId; 
   const placeName = req.params.place;
 
     if(!placeName){
           res.render('error', { layout: 'error' });
       }
-      
+
   queries
     .getPlace(placeName)
     .then((placeDetails) => {
       if(placeDetails.length === 0){
           res.render('error', { layout: 'error' });
-      } else {
-        const placeResults = placeDetails[0]
-        const placeResultsId = placeDetails[0].id
-        queries.getValues(placeResultsId)
-          .then((getValueResult) => {
+      } 
+      else {
+         placeResults = placeDetails[0];
+         placeResultsId = placeDetails[0].id;
+      }
+    })
+    .catch();
+
+  queries
+    .getValues(placeResultsId)
+    .then((getValueResult) => {
             const values = getValueResult[0].standard;
             res.render('placeDetails', { placeResults, values, layout: 'navHome' })
           })
-          .catch((err) => {
-            console.log(err);
-          })
-      }
-    })
-
     .catch((err) => {
-      console.log(err);
-    })
-};
+      res.render('error', { layout: 'error' });
+          })
+    }
