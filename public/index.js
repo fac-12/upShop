d96218a7c3d1580ca2bde625c129a6384050b085
caralemony <carolinelappleby@gmail.com>
2018-01-16 11:07:20 +0000

    geolocation functionality

--- public/index.js
+++ public/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable */
 var categoryBtn = document.getElementsByClassName('categoryBtn');
 var categoryBtnArr = Array.from(categoryBtn);
+var getLocation = document.getElementById('getLocation');
+var postcode = document.getElementById('postcode');
+
 
 categoryBtnArr.forEach(function(button){
   button.addEventListener('click', function(e){
@@ -14,3 +18,25 @@ categoryBtnArr.forEach(function(button){
     window.location.href='/search/'+ catObj[e.target.textContent];
  })
 });
+
+getLocation.addEventListener('click', function() {
+  function success(pos) {
+  fetch('http://api.postcodes.io/postcodes?lon='+pos.coords.longitude+'&lat='+pos.coords.latitude)
+         .then(response => response.json()
+             )
+            .then(data => postcode.value = data.result[0].postcode
+          )
+          .catch(error => console.log('error is', error))
+
+
+  }
+  function error(err) {
+    console.log('error= ', err);
+  }
+var options = {
+  enableHighAccuracy: true,
+  timeout: 50000,
+  maximumAge: 0
+}
+  navigator.geolocation.getCurrentPosition(success, error, options)
+})
