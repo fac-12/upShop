74e80c59d2e77c49de1acd98950d20091a66abe7
Lucy King <lucy.m.king92@gmail.com>
2018-01-17 00:14:38 +0000

    map is now populated with data

--- public/map.js
+++ public/map.js
@@ -4,7 +4,8 @@ var mapImg = document.getElementById('mapImg');
 var mapDiv = document.getElementById('mapid');
 var listResults = document.getElementById('listResults');
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var ll = resultsArr[resultsArr.length-1];
+var mymap = L.map('mapid').setView(ll, 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -13,6 +14,17 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoibHVjeW1rIiwiYSI6ImNqY2hxOXp0ODJiOW8yeHBnMmxtMjZuNnAifQ.tsRJmfxB-VdGws5WHp0yhQ'
 }).addTo(mymap);
 
+resultsArr.forEach(function(place, i){
+    if(place.lat_long) {
+        var latlong = place.lat_long.slice(1, -1);
+        var llArr = latlong.split(",");
+        llArr[0] = (llArr[0] * 1);
+        llArr[1] = (llArr[1] * 1);
+        console.log(llArr);
+        var marker = L.marker(llArr).addTo(mymap);
+        marker.bindPopup(place.name + ", " + place.address)
+    }
+})
 
 listImg.addEventListener("click", function(e){
     e.preventDefault();
@@ -30,5 +42,3 @@ mapImg.addEventListener("click", function(e){
     mapDiv.style.display = "block";
     mymap.invalidateSize();
 })
-
- console.log(resultsArr);
--- src/controllers/category.js
+++ src/controllers/category.js
@@ -4,18 +4,32 @@ const queries = require('./queries');
 
 exports.get = (req,res) => {
   const cat = req.query.category;
+  const currentPC = (req.query.postcode).replace(" ", "").toString();
 
-  // const location = req.params.location;
-
-//query db with "cat" & location
-queries
-    .catResults(cat)
-    .catch((err) => {
-      console.log(err);
+  fetch(`http://api.postcodes.io/postcodes/${currentPC}`)
+    .then((response) => {
+      if (response.status >= 400) {
+        const errorMessage = 'This postcode does not exist';
+        throw new Error(errorMessage)
+      } else {
+        return response.json();
+      }
     })
-    .then(resultsArr => {
+    .then((data) => {
+      queries.catResults(cat)
+      .then((resultsArr) => {
+        var currentLL = [data.result.latitude, data.result.longitude];
+        resultsArr.push(currentLL);
+        console.log(resultsArr);
       res.render('listView', {
           resultsArr, layout: 'list', helpers: { json: function (context) { return JSON.stringify(context); } }
         });
+      });
     })
+    .catch(err => {
+      res.render('error', { err,
+        layout: 'error',
+    })
+  });
 };
+
